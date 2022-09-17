256d252073432704fca87a83e76a560d11862d32
DevFC-M <felipech3344@gmail.com>
2022-09-17 18:46:29 -0300

    Criação das funcionalidades das rotas /games e /ads com prisma

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,56 @@
 import express, { response }  from "express";
+import { PrismaClient } from '@prisma/client'
 
 const app = express();
+const prisma = new PrismaClient({
+    log: ['query']
+});
 
-app.get('/games', (request, response) => {
-    return response.json([])
+app.get('/games', async (request, response) => {
+    const games = await prisma.game.findMany({
+        include: {
+            _count: {
+                select: {
+                    ads: true
+                }
+            }
+        }
+    })
+
+    return response.json(games)
 });
 
 app.post('/ads', (request, response) => {
     return response.status(201).json([])
 });
 
-app.get('/games/:id/ads', (request, response) => {
-    // const gameId = request.params.id
+app.get('/games/:id/ads', async (request, response) => {
+    const gameId = request.params.id;
 
-    return response.json([
-        {id: 1, name: 'Anuncio 1'},
-        {id: 2, name: 'Anuncio 2'},
-        {id: 3, name: 'Anuncio 3'},
-        {id: 3, name: 'Anuncio 4'},
-        {id: 3, name: 'Anuncio 5'},
-    ])
+    const ads = await prisma.ad.findMany({
+        select: {
+            id: true,
+            name: true,
+            weekDays: true,
+            useVoiceChannel: true, 
+            yearsPlaying: true, 
+            hourStart: true, 
+            hourEnd: true,
+        },
+        where: {
+            gameId,
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+
+    return response.json(ads.map(ad => {
+        return {
+            ...ad, 
+            weekDays: ad.weekDays.split(',')
+        }
+    }))
 })
 
 app.get('/ads/:id/discord', (request, response) => {
